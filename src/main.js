e0c14f354c9877335cf2183c70eaf7b5228200fe
Nat <nat@cmmc.io>
2019-06-11 15:48:32 +0700

    add netpie credential

--- src/main.js
+++ src/main.js
@@ -12,6 +12,30 @@ Vue.config.productionTip = false
 Vue.use(Vuex)
 Vue.use(VueRouter)
 Vue.prototype.$api = API
+Vue.prototype.$Microgear = window.Microgear
+let microgear = window.Microgear.create({
+  key: '301bT7j6D1lkIq9',
+  secret: 'QWhZCzsre0ggXdtuH7Jy0xU8w',
+})
+
+console.log(`main.js`)
+microgear.on('message', (topic, msg) => {
+  console.log(`on_message arrived`, topic, msg)
+})
+microgear.on('connected', () => {
+  //microgear.setAlias(ALIAS);
+  console.log(`on_connected to netpie.`)
+})
+microgear.on('present', event => {
+  console.log(`on_present`, event)
+})
+microgear.on('absent', event => {
+  console.log(`on_absent`, event)
+})
+
+microgear.connect('vuejs')
+
+Vue.prototype.$mg = microgear
 
 const router = new VueRouter({ routes })
 const store = new Vuex.Store(vuexStore)
